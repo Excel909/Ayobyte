22eb048561a32a51903dd22b98c4861c5f380b2e
Excellent <excellentayomide@gmail.com>
2024-05-06 17:08:32 +0100

    started media queries phase one

--- text-writer.js
+++ text-writer.js
@@ -1,34 +1,38 @@
-const sentence = "Hi am Excel and welcome to my Website!";
-  const container = document.getElementById("text-container");
-  
-  function animateText() {
+const sentences = ["Ayobyte", "Where Creativity meets functionality."];
+const container = document.getElementById("text-container");
+
+let currentSentenceIndex = 0;
+
+function animateText() {
     let index = 0;
+    const sentence = sentences[currentSentenceIndex];
     const interval = setInterval(() => {
-      if (index <= sentence.length) {
-        container.innerHTML = sentence.substring(0, index) + "<b>" + sentence.charAt(index) + "</b>";
-        index++;
-      } else {
-        clearInterval(interval);
-        setTimeout(() => {
-          deleteText();
-        }, 1000); // Delay before deleting
-      }
+        if (index <= sentence.length) {
+            container.innerHTML = sentence.substring(0, index) + "<b>" + sentence.charAt(index) + "</b>";
+            index++;
+        } else {
+            clearInterval(interval);
+            setTimeout(() => {
+                deleteText();
+            }, 1000); // Delay before deleting
+        }
     }, 50); // Speed of typing in milliseconds
-  }
+}
 
-  function deleteText() {
-    let index = sentence.length;
+function deleteText() {
+    let index = sentences[currentSentenceIndex].length;
     const interval = setInterval(() => {
-      if (index >= 0) {
-        container.innerHTML = sentence.substring(0, index);
-        index--;
-      } else {
-        clearInterval(interval);
-        setTimeout(() => {
-          animateText();
-        }, 1000); // Delay before typing again
-      }
+        if (index >= 0) {
+            container.innerHTML = sentences[currentSentenceIndex].substring(0, index);
+            index--;
+        } else {
+            clearInterval(interval);
+            setTimeout(() => {
+                currentSentenceIndex = (currentSentenceIndex + 1) % sentences.length; // Move to the next sentence
+                animateText(); // Start typing the next sentence
+            }, 1000); // Delay before typing the next sentence
+        }
     }, 50); // Speed of deleting in milliseconds
-  }
+}
 
-  animateText();
+animateText(); // Start the animation
